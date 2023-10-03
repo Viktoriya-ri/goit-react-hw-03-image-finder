c9deb122d39bcaacd00d3c71a10b6f730287db53
Вікторія <viktoriya23071@gmail.com>
2023-10-03 10:22:29 +0300

    loader, modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,3 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ApiService from './pixabayApi/PostApiService';
+import Loader from './Loader/Loader';
